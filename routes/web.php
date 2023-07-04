fbe3a6e2771a6612efa070e5f543b19d7849cb9c
Trichine Boubakeur <trichine aboubakeur>
2023-07-05 00:58:55 +0200

    Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', function () {
+    return 'Homepage';
+});
+
+Route::get('/product', function () {
+    return 'Liste des produits';
+});
+
+Route::get('/product/{id}', function ($id) {
+    return "Fiche du produit $id";
+});
+
+Route::get('/cart', function () {
+    return 'Panier';
+});
